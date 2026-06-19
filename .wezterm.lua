baf82f868c6a79216540ed15ccbcacb732116bda
Mike Shamasa <mshamasa@gmail.com>
2026-06-18 22:05:05 -0700

    chore: update wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,19 +1,34 @@
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font_size = 11
-config.font = wezterm.font('MesloLGS NF')
-config.color_scheme = 'tokyonight_night'
+config.font = wezterm.font("MesloLGS NF")
+config.color_scheme = "tokyonight_night"
 config.initial_cols = 180
 config.initial_rows = 55
 
 config.keys = {
-  {
-    key = 'w',
-    mods = 'CMD',
-    action = wezterm.action.CloseCurrentPane { confirm = false },
-  }
+	{
+		key = "w",
+		mods = "CMD",
+		action = wezterm.action.CloseCurrentPane({ confirm = false }),
+	},
+	-- This will create a new split and run your default program inside it
+	{
+		key = "/",
+		mods = "CMD",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "k",
+		mods = "CMD",
+		action = wezterm.action.ActivatePaneDirection("Next"),
+	},
+	{
+		key = "j",
+		mods = "CMD",
+		action = wezterm.action.ActivatePaneDirection("Prev"),
+	},
 }
-
 
 return config
